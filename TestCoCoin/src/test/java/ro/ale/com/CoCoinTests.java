12a2e1d4583c88edd901769805a3ec0f2252348b
Alexandra Casapu <heirloomalexis@gmail.com>
2018-03-24 21:26:57 +0200

    setup appium driver

--- TestCoCoin/src/test/java/ro/ale/com/CoCoinTests.java
+++ TestCoCoin/src/test/java/ro/ale/com/CoCoinTests.java
@@ -1,4 +1,45 @@
 package ro.ale.com;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class CoCoinTests {
+
+    AppiumDriver driver;
+
+
+
+    @BeforeClass
+    void getDriver() throws MalformedURLException {
+
+
+        File appPath = new File("CoCoin.apk");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("app", appPath);
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("appWaitActivity", "com.nightonke.saver.activity.MainActivity");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @AfterClass
+    void tearDown() {
+    }
+
+    @Test
+    void sampleTest() {
+
+    }
 }
